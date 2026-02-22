74fe0df9da0e88162a0ee36f418e5c6cbca1ae3e
Martin Larsson <larssonmartin1998@gmail.com>
2026-02-23 00:48:34 +0100

    fix: port flag no longer always remain 8080

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -10,7 +10,7 @@ import (
 )
 
 type CliArgs struct {
-	PwFilePath    string
+	PwFilePath string
 	ConfigPath string
 	Port       uint16
 }
@@ -19,17 +19,17 @@ func ParseArgs() *CliArgs {
 	configPath := flag.String("config-path", "config.toml", "Path to the configuration file, defaults to './config.toml'")
 	pwFilePath := flag.String("pw-file", "", "Path to the password file, if run without a password file, auth token middleware will be disabled.")
 
-	portFlag := *flag.Uint64("port", 8080, "The port that the HTTP server will listen on")
+	portFlag := flag.Uint64("port", 8080, "The port that the HTTP server will listen on")
 	flag.Parse()
 
-	if portFlag > math.MaxUint16 {
+	if *portFlag > math.MaxUint16 {
 		slog.Error("--port flag is too high.", "max value", math.MaxUint16, "got", portFlag)
 		os.Exit(apperror.CodeInvalidCliArgument)
 	}
 
 	return &CliArgs{
-		PwFilePath:    *pwFilePath,
+		PwFilePath: *pwFilePath,
 		ConfigPath: *configPath,
-		Port:       uint16(portFlag),
+		Port:       uint16(*portFlag),
 	}
 }
