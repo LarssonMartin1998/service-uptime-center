2b0f084c195750b3816da9259078c8603eeae384
Martin Larsson <larssonmartin1998@gmail.com>
2026-02-22 01:29:24 +0100

    remove unused sleepduration from cli args

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -7,20 +7,17 @@ import (
 	"math"
 	"os"
 	"service-uptime-center/internal/app/apperror"
-	"time"
 )
 
 type CliArgs struct {
-	ConfigPath    string
-	Port          uint16
 	PwFilePath    string
-	SleepDuration time.Duration
+	ConfigPath string
+	Port       uint16
 }
 
 func ParseArgs() *CliArgs {
 	configPath := flag.String("config-path", "config.toml", "Path to the configuration file, defaults to './config.toml'")
 	pwFilePath := flag.String("pw-file", "", "Path to the password file, if run without a password file, auth token middleware will be disabled.")
-	sleepDurationStr := flag.String("sleep-duration", "3h", "How frequently to check for missing pulses from services.")
 
 	portFlag := *flag.Uint64("port", 8080, "The port that the HTTP server will listen on")
 	flag.Parse()
@@ -30,16 +27,9 @@ func ParseArgs() *CliArgs {
 		os.Exit(apperror.CodeInvalidCliArgument)
 	}
 
-	duration, err := time.ParseDuration(*sleepDurationStr)
-	if err != nil {
-		slog.Error("--sleep-duration has invalid format (supported format: ns, us, ms, s, m, h)")
-		os.Exit(apperror.CodeInvalidCliArgument)
-	}
-
 	return &CliArgs{
-		ConfigPath:    *configPath,
-		Port:          uint16(portFlag),
 		PwFilePath:    *pwFilePath,
-		SleepDuration: duration,
+		ConfigPath: *configPath,
+		Port:       uint16(portFlag),
 	}
 }
